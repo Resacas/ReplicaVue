da328bc4f031dbd2bb78d90485e0699f7ff6ab10
Resacas <73529170+Resacas@users.noreply.github.com>
2021-03-10 08:46:05 +0100

    a

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,41 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import NavBar from '@/components/NavBar.vue'
+import Data from '@/data/data.json'
 
-describe('M.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+
+// describe('M.vue', () => {
+//   it('renders props.msg when passed', () => {
+//     const msg = 'new message'
+//     const wrapper = shallowMount(HelloWorld, {
+//       props: { msg }
+//     })
+//     expect(wrapper.text()).toMatch(msg)
+//   })
+// })
+
+
+test('renders id param', () => {
+  const wrapper = shallowMount(NavBar, {
+    mocks: {
+      $route: {
+        params: {
+          route: '/'
+        }
+      }
+    }
   })
+  expect(wrapper.text()).toContain('PRODUCTS')
+})
+test('renders id param', () => {
+  const wrapper = shallowMount(NavBar, {
+    mocks: {
+      $route: {
+        params: {
+          route: '/'
+        }
+      }
+    }
+  })
+  expect(typeof wrapper.text()).toBe('string')
 })
